bdfb54c91d11b57365c52647eb315e78c4921c8e
dbwlsgur9811 <dbwlsgur9811@gmail.com>
2022-05-12 15:30:03 +0900

    ADD : JiHoo파일 추가

---
+++ src/pages/JiHoo/Main/MainJiHoo.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import './MainJiHoo.scss';
+const MainJiHoo = () => {
+  return (
+    <div class="JiHoo">
+      <section>
+        <nav>
+          <div className="header">
+            <div className="logo">
+              <i className="fa-brands fa-instagram fa-1x"></i>
+              <span className="title">Westagram</span>
+            </div>
+            <input type="text" className="search" placeholder="검색" />
+            <div className="icon">
+              <img src="  /images/JiHoo/explore.png" alt="explore" />
+              <img src="  /images/JiHoo/heart.png" alt="heart" />
+              <img src="  /images/JiHoo/profile.png" alt="profile" />
+            </div>
+          </div>
+        </nav>
+        <main>
+          <div className="feeds">
+            <article>
+              <div className="userInfor">
+                <img
+                  className="usericon"
+                  src="/images/JiHoo/usericon.png"
+                  alt="usericon"
+                />
+                <span className="nickName">jihoo</span>
+                <img className="more" src="/images/JiHoo/more.PNG" alt="more" />
+              </div>
+              <img
+                className="img"
+                src="/images/JiHoo/icecream.jpg"
+                alt="icecream"
+              />
+              <div className="feed">
+                <div className="feed_icons">
+                  <span className="like_icon">
+                    <i className="fa-solid fa-heart fa-2x"></i>
+                  </span>
+                  <span className="comment_icon">
+                    <i className="fa-regular fa-comment fa-2x"></i>{' '}
+                  </span>
+                  <span className="send_icon">
+                    <i className="fa-regular fa-paper-plane fa-2x"></i>
+                  </span>
+                  <span className="bookmark_icon">
+                    <i className="fa-regular fa-bookmark fa-2x"></i>
+                  </span>
+                </div>
+                <div className="feed_content">
+                  <div className="like">
+                    <img
+                      className="user_icon"
+                      src="/images/JiHoo/usericon.PNG"
+                      alt="usericon"
+                    />
+                    <span className="name">jihoo</span>님{' '}
+                    <span className="name">외 306명</span>이 좋아합니다
+                  </div>
+                  <div className="comment">
+                    <span className="name">jihoo</span> 청담 베스킨라빈스
+                    브라운에서~~ 더보기<br></br>
+                    <span className="name">dlwlrma</span> 다음에 같이가자~~~
+                    <br></br> 1시간전
+                  </div>
+                  <div className="comment_upload">
+                    <input
+                      className="commentUpload"
+                      type="text"
+                      placeholder="댓글 달기..."
+                    />
+                    <button className="btn" disabled="disabled">
+                      게시
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div>
+          <div className="main-right">
+            <div className="users">
+              <img
+                src="/images/JiHoo/usericon2.PNG"
+                className="storyicon"
+                alt="usericon2"
+              />
+              <span className="jihoo">jihoo1</span>
+              <span className="follow">팔로우</span>
+            </div>
+            <div className="users">
+              <img
+                src="/images/JiHoo/usericon2.PNG"
+                className="storyicon"
+                alt="usericon2"
+              />
+              <span className="jihoo">jihoo2</span>
+              <span className="follow">팔로우</span>
+              {''}
+            </div>
+            <div className="users">
+              <img
+                src="/images/JiHoo/usericon2.PNG"
+                className="storyicon"
+                alt="usericon2"
+              />
+              <span className="jihoo">jihoo3</span>
+              <span className="follow">팔로우</span>
+            </div>
+            <div className="users">
+              <img
+                src="/images/JiHoo/usericon2.PNG"
+                className="storyicon"
+                alt="usericon2"
+              />
+              <span className="jihoo">jihoo4</span>
+              <span className="follow">팔로우</span>
+            </div>
+            <div className="users">
+              <img
+                src="/images/JiHoo/usericon2.PNG"
+                className="storyicon"
+                alt="usericon2"
+              />
+              <span className="jihoo">jihoo5</span>
+              <span className="follow">팔로우</span>
+            </div>
+          </div>
+        </main>
+      </section>
+    </div>
+  );
+};
+
+export default MainJiHoo;
