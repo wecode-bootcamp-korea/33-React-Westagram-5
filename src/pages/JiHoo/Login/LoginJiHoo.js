e8c6e5eafd2b78ec41d6e2f9e23128001f05d0a6
dbwlsgur9811 <dbwlsgur9811@gmail.com>
2022-05-13 16:20:54 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/JiHoo/Login/LoginJiHoo.js
+++ src/pages/JiHoo/Login/LoginJiHoo.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LoginJiHoo.scss';
 
 function LoginJiHoo() {
-  const navigate = useNavigate();
+  const [isId, setIsId] = useState();
+  const [isPassWord, setIsPassWord] = useState();
 
+  const handleIdInput = e => {
+    setIsId(e.target.value);
+  };
+  const handlePwInput = e => {
+    setIsPassWord(e.target.value);
+  };
+
+  const navigate = useNavigate();
   const goToMain = () => {
     navigate('/main-JiHoo');
   };
+
   return (
     <div>
       <body>
@@ -19,11 +29,13 @@ function LoginJiHoo() {
                 type="text"
                 className="id"
                 placeholder="전화번호, 사용자 이름 또는 이메일"
+                onChange={handleIdInput}
               />
               <input
                 type="password"
                 className="passWord"
                 placeholder="비밀번호"
+                onChange={handlePwInput}
               />
               <button className="login_Btn" onClick={goToMain}>
                 로그인
