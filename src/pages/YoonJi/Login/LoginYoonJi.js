89b3976a2d68c227e6808a44c42115b8abc84331
sourmix <75124027+sourmix@users.noreply.github.com>
2022-05-15 21:01:43 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/YoonJi/Login/LoginYoonJi.js
+++ src/pages/YoonJi/Login/LoginYoonJi.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import './LoginYoonJi.scss';
 import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from "react";
-
+import { useState, useEffect } from 'react';
 
 function LoginYoonji() {
   const navigate = useNavigate();
-
   const goToMain = () => {
     navigate('/main-yoonji');
   };
+
+  const [id, setId] = useState('');
+  const handleIdInput = event => setId(event.target.value);
+  const [pw, setPw] = useState('');
+  const handlePwInput = event => setPw(event.target.value);
+
   return (
     <>
       <div className="container">
         <div className="header">Westagram</div>
         <form>
           <input
+            onChange={handleIdInput}
             type="text"
             name=""
             id="userId"
             placeholder="전화번호, 사용자 이름 또는 이메일"
           />
-          <input type="password" name="" id="userPwd" placeholder="비밀번호" />
+          <input
+            onChange={handlePwInput}
+            type="password"
+            name=""
+            id="userPwd"
+            placeholder="비밀번호"
+          />
           <button onClick={goToMain}>로그인</button>
         </form>
         <div className="footer">비밀번호를 잊으셨나요?</div>
